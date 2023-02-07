70a1857cdf9449a46913fc5788870c166a4fd183
seflaks <spk.lotos@gmail.com>
2023-02-07 17:11:24 +0300

    HW01 add вывод таблицы приема пищи

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,9 +16,9 @@ public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
-
-        response.sendRedirect("meals.jsp");
+        request.setAttribute("mealsTo", MealsUtil.getListMealTo());
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html lang="ru">
 <head>
     <title>Meals</title>
@@ -7,5 +8,19 @@
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+<table border = "1" cellspacing="0" cellpadding="8">
+    <tr>
+        <td>DateTime</td>
+        <td>Description</td>
+        <td>Calories</td>
+    </tr>
+    <c:forEach var="mealTo" items="${requestScope.mealsTo}" >
+        <tr style = "color:${mealTo.excess ? "red" : "green"};">
+            <td>${mealTo.dateTime}</td>
+            <td>${mealTo.description}</td>
+            <td>${mealTo.calories}</td>
+        </tr>
+</c:forEach>
+</table>
 </body>
 </html>
