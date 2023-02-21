9b2fff328fc945268f68f46754a08822d8cc0cd0
Seflaks <spk.lotos@gmail.com>
2023-02-21 20:58:03 +0300

    HW03 add sql-scripts create table and fill table meals

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user_role;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -15,6 +16,17 @@ CREATE TABLE users
     calories_per_day INTEGER             DEFAULT 2000  NOT NULL
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+
+CREATE TABLE meals
+(
+    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    user_id     INTEGER     NOT NULL,
+    date_time   TIMESTAMP   NOT NULL,
+    description VARCHAR     NOT NULL,
+    calories    INTEGER     NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX meals_unique_user_date_time_idx ON meals (user_id, date_time);
 
 CREATE TABLE user_role
 (
