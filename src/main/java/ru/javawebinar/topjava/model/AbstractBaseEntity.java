aa9399e53bde66f1d8c906fcc809e5770af8b1d6
seflaks <spk.lotos@gmail.com>
2023-02-24 21:28:13 +0300

    4_8_add_jpa+

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -2,6 +2,11 @@ package ru.javawebinar.topjava.model;
 
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@MappedSuperclass
+// http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
+@Access(AccessType.FIELD)
 public abstract class AbstractBaseEntity {
     public static final int START_SEQ = 100000;
 
